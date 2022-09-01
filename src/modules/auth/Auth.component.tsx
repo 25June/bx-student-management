6680f01e9adc63cdd6f5c648273f1c301dd78996
phunguyen <ngocphu2506@gmail.com>
2022-09-01 19:36:00 +0700

    sign in method

--- src/modules/auth/Auth.component.tsx
+++ src/modules/auth/Auth.component.tsx
@@ -1,77 +1,18 @@
-import { UserCredential } from 'firebase/auth'
-import { auth } from '../../firebase'
+import { Box, Button } from '@mui/material'
 import React, { useState } from 'react'
-import {
-  Box,
-  TextField,
-  InputLabel,
-  OutlinedInput,
-  InputAdornment,
-  IconButton,
-  Snackbar,
-  Alert,
-  AlertProps,
-  Typography,
-  Button,
-} from '@mui/material'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
-import { useForm, Controller } from 'react-hook-form'
-import VisibilityOff from '@mui/icons-material/VisibilityOff'
-import Visibility from '@mui/icons-material/Visibility'
+import SignInComponent from './SignIn.component'
+import SignUpComponent from './SignUp.component'
 
-type SignInForm = {
-  email: string
-  password: string
-  confirmPassword: string
+enum AuthenticationMethod {
+  SIGN_IN = 'SIGN_IN',
+  SIGN_UP = 'SIGN_UP',
+  FORGOT_PASSWORD = 'FORGOT_PASSWORD',
 }
 
 const AuthComponent = () => {
-  const { control, handleSubmit } = useForm<SignInForm>({
-    defaultValues: {
-      email: '',
-      password: '',
-      confirmPassword: '',
-    },
-  })
-  const [showPassword, setShowPassword] = useState<boolean>(false)
-  const [showConfirmPassword, setShowConfirmPassword] = useState<boolean>(false)
-  const [openSnackbar, setOpenSnackbar] = useState<boolean>(false)
-  const [snackBarContent, setSnackbarContent] = useState<{
-    severity: AlertProps['severity']
-    message: string
-  }>({
-    severity: 'success',
-    message: '',
-  })
-
-  const onSubmit = (values: SignInForm) => {
-    if (values.password !== values.confirmPassword) {
-      setSnackbarContent({ severity: 'error', message: `Mật khẩu không trùng khớp` })
-      setOpenSnackbar(true)
-    }
-    console.log(values)
-    createUserWithEmailAndPassword(auth, values.email, values.password)
-      .then(({ user }: UserCredential) => {
-        console.log(user)
-        setSnackbarContent({ severity: 'success', message: `${user.email} Đăng nhập thành công` })
-        setOpenSnackbar(true)
-      })
-      .catch((error) => {
-        console.error(error)
-        setSnackbarContent({ severity: 'error', message: `Đăng nhập thất bại` })
-        setOpenSnackbar(true)
-      })
-  }
-
-  const handleClickShowPassword = () => setShowPassword(!showPassword)
-  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault()
-  }
-
-  const handleClickShowConfirmPassword = () => setShowConfirmPassword(!showPassword)
-  const handleMouseDownConfirmPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault()
-  }
+  const [currentMethod, setCurrentMethod] = useState<AuthenticationMethod>(
+    AuthenticationMethod.SIGN_IN
+  )
 
   return (
     <Box
@@ -82,103 +23,18 @@ const AuthComponent = () => {
       alignItems={'center'}
       textAlign={'left'}
     >
-      <Box width={500} border={'1px solid #F0F0F0'} padding={2}>
-        <Typography variant={'h3'}>Đăng ký</Typography>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Controller
-            rules={{ required: true }}
-            control={control}
-            name={'email'}
-            render={({ field }) => {
-              return (
-                <TextField
-                  label={'Nhập email'}
-                  variant={'outlined'}
-                  helperText={'abc@gmail.com'}
-                  margin={'normal'}
-                  type={'email'}
-                  fullWidth={true}
-                  {...field}
-                />
-              )
-            }}
-          />
-          <Controller
-            rules={{ required: true }}
-            control={control}
-            name={'password'}
-            render={({ field }) => {
-              return (
-                <>
-                  <InputLabel htmlFor={'outlined-adornment-password'}>Mật khẩu</InputLabel>
-                  <OutlinedInput
-                    id={'outlined-adornment-password'}
-                    type={showPassword ? 'text' : 'password'}
-                    fullWidth={true}
-                    {...field}
-                    endAdornment={
-                      <InputAdornment position="end">
-                        <IconButton
-                          aria-label="toggle password visibility"
-                          onClick={handleClickShowPassword}
-                          onMouseDown={handleMouseDownPassword}
-                          edge="end"
-                        >
-                          {showPassword ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                      </InputAdornment>
-                    }
-                  />
-                </>
-              )
-            }}
-          />
-          <Controller
-            rules={{ required: true }}
-            control={control}
-            name={'confirmPassword'}
-            render={({ field }) => {
-              return (
-                <>
-                  <InputLabel htmlFor={'outlined-adornment-confirm-password'}>
-                    Xác nhận lại mật khẩu
-                  </InputLabel>
-                  <OutlinedInput
-                    id={'outlined-adornment-confirm-password'}
-                    type={showConfirmPassword ? 'text' : 'password'}
-                    fullWidth={true}
-                    {...field}
-                    endAdornment={
-                      <InputAdornment position="end">
-                        <IconButton
-                          aria-label="toggle password visibility"
-                          onClick={handleClickShowConfirmPassword}
-                          onMouseDown={handleMouseDownConfirmPassword}
-                          edge="end"
-                        >
-                          {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
-                        </IconButton>
-                      </InputAdornment>
-                    }
-                  />
-                </>
-              )
-            }}
-          />
-          <Button type={'submit'} onSubmit={handleSubmit(onSubmit)}>
-            Đăng nhập
-          </Button>
-        </form>
-      </Box>
-      <Snackbar open={openSnackbar} autoHideDuration={5000} onClose={() => setOpenSnackbar(false)}>
-        <Alert
-          onClose={() => setOpenSnackbar(false)}
-          severity={snackBarContent.severity}
-          sx={{ width: '100%' }}
-        >
-          {snackBarContent.message}
-        </Alert>
-      </Snackbar>
+      {currentMethod === AuthenticationMethod.SIGN_IN && (
+        <>
+          <SignInComponent />
+          <Button onClick={() => setCurrentMethod(AuthenticationMethod.SIGN_UP)}>Đăng ký</Button>
+        </>
+      )}
+      {currentMethod === AuthenticationMethod.SIGN_UP && (
+        <>
+          <SignUpComponent />
+          <Button onClick={() => setCurrentMethod(AuthenticationMethod.SIGN_IN)}>Đăng nhập</Button>
+        </>
+      )}
     </Box>
   )
 }
---
+++ src/modules/auth/SignUp.component.tsx
@@ -0,0 +1,178 @@
+import { Controller, useForm } from 'react-hook-form'
+import React, { useState } from 'react'
+import {
+  Alert,
+  AlertProps,
+  Box,
+  Button,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Snackbar,
+  TextField,
+  Typography,
+} from '@mui/material'
+import { createUserWithEmailAndPassword, UserCredential } from 'firebase/auth'
+import { auth } from '../../firebase'
+import VisibilityOff from '@mui/icons-material/VisibilityOff'
+import Visibility from '@mui/icons-material/Visibility'
+
+type SignUpForm = {
+  email: string
+  password: string
+  confirmPassword: string
+}
+
+const SignUpComponent = () => {
+  const { control, handleSubmit } = useForm<SignUpForm>({
+    defaultValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+  })
+  const [showPassword, setShowPassword] = useState<boolean>(false)
+  const [showConfirmPassword, setShowConfirmPassword] = useState<boolean>(false)
+  const [openSnackbar, setOpenSnackbar] = useState<boolean>(false)
+  const [snackBarContent, setSnackbarContent] = useState<{
+    severity: AlertProps['severity']
+    message: string
+  }>({
+    severity: 'success',
+    message: '',
+  })
+
+  const onSubmit = (values: SignUpForm) => {
+    if (values.password !== values.confirmPassword) {
+      setSnackbarContent({ severity: 'error', message: `Mật khẩu không trùng khớp` })
+      setOpenSnackbar(true)
+    }
+    console.log(values)
+    createUserWithEmailAndPassword(auth, values.email, values.password)
+      .then(({ user }: UserCredential) => {
+        console.log(user)
+        setSnackbarContent({ severity: 'success', message: `${user.email} Đăng nhập thành công` })
+        setOpenSnackbar(true)
+      })
+      .catch((error) => {
+        console.error(error)
+        setSnackbarContent({ severity: 'error', message: `Đăng nhập thất bại` })
+        setOpenSnackbar(true)
+      })
+  }
+
+  const handleClickShowPassword = () => setShowPassword(!showPassword)
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+  }
+
+  const handleClickShowConfirmPassword = () => setShowConfirmPassword(!showPassword)
+  const handleMouseDownConfirmPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+  }
+
+  return (
+    <Box>
+      <Box width={500} border={'1px solid #F0F0F0'} padding={2}>
+        <Typography variant={'h3'}>Đăng ký</Typography>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Controller
+            rules={{ required: true }}
+            control={control}
+            name={'email'}
+            render={({ field }) => {
+              return (
+                <TextField
+                  label={'Nhập email'}
+                  variant={'outlined'}
+                  helperText={'abc@gmail.com'}
+                  margin={'normal'}
+                  type={'email'}
+                  fullWidth={true}
+                  {...field}
+                />
+              )
+            }}
+          />
+          <Controller
+            rules={{ required: true }}
+            control={control}
+            name={'password'}
+            render={({ field }) => {
+              return (
+                <>
+                  <InputLabel htmlFor={'outlined-adornment-password'}>Mật khẩu</InputLabel>
+                  <OutlinedInput
+                    id={'outlined-adornment-password'}
+                    type={showPassword ? 'text' : 'password'}
+                    fullWidth={true}
+                    {...field}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          onMouseDown={handleMouseDownPassword}
+                          edge="end"
+                        >
+                          {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                </>
+              )
+            }}
+          />
+          <Controller
+            rules={{ required: true }}
+            control={control}
+            name={'confirmPassword'}
+            render={({ field }) => {
+              return (
+                <>
+                  <InputLabel htmlFor={'outlined-adornment-confirm-password'}>
+                    Xác nhận lại mật khẩu
+                  </InputLabel>
+                  <OutlinedInput
+                    id={'outlined-adornment-confirm-password'}
+                    type={showConfirmPassword ? 'text' : 'password'}
+                    fullWidth={true}
+                    {...field}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowConfirmPassword}
+                          onMouseDown={handleMouseDownConfirmPassword}
+                          edge="end"
+                        >
+                          {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                </>
+              )
+            }}
+          />
+          <Button type={'submit'} onSubmit={handleSubmit(onSubmit)}>
+            Đăng nhập
+          </Button>
+        </form>
+      </Box>
+      <Snackbar open={openSnackbar} autoHideDuration={5000} onClose={() => setOpenSnackbar(false)}>
+        <Alert
+          onClose={() => setOpenSnackbar(false)}
+          severity={snackBarContent.severity}
+          sx={{ width: '100%' }}
+        >
+          {snackBarContent.message}
+        </Alert>
+      </Snackbar>
+    </Box>
+  )
+}
+
+export default SignUpComponent
