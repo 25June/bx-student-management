5f1efeae0bfb1c023f2547ac85ca07b84ec128a0
phunguyen <ngocphu2506@gmail.com>
2023-02-25 11:16:00 +0700

    assessment

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,7 +1,12 @@
 import { useUpdateStudent, useDeleteStudent, useAddNewStudent, useGetStudents } from './student'
 import { studentRef, queryClasses } from './firestore'
 import { uploadAvatar, uploadProgress, removeImage } from 'services/storage'
-import { useAddNewAssessment, useGetAssessments, useEditAssessment, useDeleteAssessment } from './assessment'
+import {
+  useAddNewAssessment,
+  useGetAssessments,
+  useEditAssessment,
+  useDeleteAssessment,
+} from './assessment'
 
 export {
   useUpdateStudent,
