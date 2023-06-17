6ad4587e5dedc0e9f9ea92be400bf11cf4fab357
phunguyen <ngocphu2506@gmail.com>
2023-06-17 09:17:45 +0700

    remove unused component

--- src/modules/diligent-table/DiligentTable.component.tsx
+++ src/modules/diligent-table/DiligentTable.component.tsx
@@ -20,7 +20,6 @@ import { Box } from '@mui/material'
 import Accordion from '@mui/material/Accordion'
 import AccordionSummary from '@mui/material/AccordionSummary'
 import AccordionDetails from '@mui/material/AccordionDetails'
-import Typography from '@mui/material/Typography'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 
 interface StudentRows extends Student {
