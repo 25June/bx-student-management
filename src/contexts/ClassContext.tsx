29ed914181cbd0137a88f2b6e051b89616fff788
phunguyen <ngocphu2506@gmail.com>
2023-09-09 12:38:16 +0700

    allow update again for all

--- src/contexts/ClassContext.tsx
+++ src/contexts/ClassContext.tsx
@@ -48,6 +48,7 @@ export const ClassProvider = ({ children }: PropsWithChildren) => {
     return user?.classId !== classId && user?.role !== Role.CTO
   }, [user, classId])
   const value = useMemo(() => {
+    console.log(disableUpdate)
     return {
       classId: isSignedIn ? classId : '',
       setClassId,
@@ -56,7 +57,7 @@ export const ClassProvider = ({ children }: PropsWithChildren) => {
       semesterId,
       setSchoolYearId,
       setSemesterId,
-      disableUpdate,
+      disableUpdate: false,
     }
   }, [classId, isSignedIn, schoolYearId, semesterId, disableUpdate])
   return <ClassContext.Provider value={value}>{children}</ClassContext.Provider>
