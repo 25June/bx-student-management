725982e3ecfa73f2a31a59011fea58db48c80a8a
phunguyen <ngocphu2506@gmail.com>
2023-11-21 00:52:27 +0700

    change background icon

--- src/modules/layout/Layout.component.tsx
+++ src/modules/layout/Layout.component.tsx
@@ -7,10 +7,12 @@ import { PropsWithChildren } from 'react'
 import { useIsMobile } from 'utils/common'
 import SpeedDialComponent from 'modules/speed-dial/SpeedDial.component'
 import UserDrawerComponent from 'modules/user-drawer/UserDrawer.component'
-import ChienConBackground from 'static/images/cards/chien-con-no-white-bg.svg'
+// import ChienConBackground from 'static/images/cards/chien-con-no-white-bg.svg'
 import { useAuthentication } from 'contexts/AuthContext'
 
 export const drawerWidth = 240
+const backgroundURL =
+  'https://firebasestorage.googleapis.com/v0/b/bx-management.appspot.com/o/email-images%2Fchristmas-tree-bg.png?alt=media&token=c45f52a4-d2d1-4eff-ad60-2b8695e9f4c7'
 
 const LayoutComponent = ({ children }: PropsWithChildren) => {
   const [isOpen, setOpen] = useState<boolean>(false)
@@ -18,9 +20,19 @@ const LayoutComponent = ({ children }: PropsWithChildren) => {
   const isMobile = useIsMobile()
   const { isSignedIn } = useAuthentication()
   if (!isSignedIn) {
-    return <Box sx={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <CircularProgress />
-    </Box>
+    return (
+      <Box
+        sx={{
+          width: '100vw',
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    )
   }
   return (
     <Box sx={{ position: 'relative' }}>
@@ -51,9 +63,9 @@ const LayoutComponent = ({ children }: PropsWithChildren) => {
 
       <Box
         component={'img'}
-        src={ChienConBackground}
+        src={backgroundURL}
         alt={'chien-con-background'}
-        sx={{ position: 'fixed', bottom: 8, right: 30, width: 200, height: 200, opacity: 0.5 }}
+        sx={{ position: 'fixed', bottom: 8, right: 30, width: 200, height: 200, opacity: 0.75 }}
       />
     </Box>
   )
