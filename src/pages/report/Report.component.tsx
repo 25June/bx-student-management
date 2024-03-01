228672ba8b1198a0aadfdc6daf428ffb0ef62e41
phunguyen <ngocphu2506@gmail.com>
2024-03-01 22:18:52 +0700

    trigger 2

--- src/pages/report/Report.component.tsx
+++ src/pages/report/Report.component.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import { useClassContext } from 'contexts/ClassContext'
 // import { useAuthentication } from 'contexts/AuthContext'
 import ExportStudentComponent from 'pages/import/ExportStudent.component'
-// import ImportComponent from 'pages/import/Import.component'
+import ImportComponent from 'pages/import/Import.component'
 import ExportDiligentComponent from 'pages/import/ExportDiligent.component'
 import ExportScoreBookComponent from 'pages/import/ExportScoreBook.component'
 
@@ -49,15 +49,15 @@ const ReportComponent = () => {
     <Box>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={tabValue} onChange={handleChange} variant="fullWidth">
-          {/* <Tab label="Nhập" value={1} disabled={user?.role !== Role.CTO} {...a11yProps(1)} /> */}
+          <Tab label="Nhập" value={1} {...a11yProps(1)} />
           <Tab label="Xuất Danh Sách" value={2} {...a11yProps(2)} />
           <Tab label="Xuất Chuyên Cần" value={3} {...a11yProps(3)} />
           <Tab label="Xuất Bảng Điểm" value={4} {...a11yProps(4)} />
         </Tabs>
       </Box>
-      {/* <CustomTabPanel value={tabValue} index={1}>
+      <CustomTabPanel value={tabValue} index={1}>
         <ImportComponent />
-      </CustomTabPanel> */}
+      </CustomTabPanel>
       <CustomTabPanel value={tabValue} index={2}>
         {currentClass && <ExportStudentComponent currentClass={currentClass} />}
       </CustomTabPanel>
